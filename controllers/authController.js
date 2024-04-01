869f16ae7f3bb0c7eb721d560d4c872e79d180f4
verhowyna <verhowyna@ukr.net>
2024-04-01 23:20:27 +0300

    small fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,63 +4,57 @@ import * as authServices from "../services/authServices.js";
 
 import HttpError from "../helpers/HttpError.js";
 
+import ctrlWrapper from "../helpers/ctrlWrapper.js";
+
 const { JWT_SECRET } = process.env;
 
-const signup = async (req, res, next) => {
-  try {
-    const { email } = req.body;
-    const user = await authServices.findUser({ email });
-    if (user) {
-      throw HttpError(409, "Email is use");
-    }
-    const newUser = await authServices.signup(req.body);
-
-    res.status(201).json({
-      user: {
-        email: newUser.email,
-        subscription: newUser.subscription,
-      },
-    });
-  } catch (error) {
-    next(error);
+const signup = async (req, res) => {
+  const { email } = req.body;
+  const user = await authServices.findUser({ email });
+  if (user) {
+    throw HttpError(409, "Email is use");
   }
+  const newUser = await authServices.signup(req.body);
+
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+    },
+  });
 };
 
 const signin = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const user = await authServices.findUser({ email });
-    if (!user) {
-      throw HttpError(401, "Email or password is wrong");
-    }
-    const comparePassword = authServices.validatePassword(
-      password,
-      user.password
-    );
-
-    if (!comparePassword) {
-      throw HttpError(401, "Email or password is wrong");
-    }
-
-    const { _id: id } = user;
-
-    const payload = {
-      id,
-    };
-
-    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
-    await authServices.updateUser({ _id: id }, { token });
-
-    res.json({
-      token,
-      user: {
-        email,
-        subscription: user.subscription,
-      },
-    });
-  } catch (error) {
-    next(error);
+  const { email, password } = req.body;
+  const user = await authServices.findUser({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
   }
+  const comparePassword = authServices.validatePassword(
+    password,
+    user.password
+  );
+
+  if (!comparePassword) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const { _id: id } = user;
+
+  const payload = {
+    id,
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+  await authServices.updateUser({ _id: id }, { token });
+
+  res.json({
+    token,
+    user: {
+      email,
+      subscription: user.subscription,
+    },
+  });
 };
 
 const getCurrent = async (req, res) => {
@@ -79,4 +73,9 @@ const signout = async (req, res) => {
   res.status(204).json();
 };
 
-export default { signup, signin, getCurrent, signout };
+export default {
+  signup: ctrlWrapper(signup),
+  signin: ctrlWrapper(signin),
+  getCurrent: ctrlWrapper(getCurrent),
+  signout: ctrlWrapper(signout),
+};
