7da554a2d553c9b45a88c52b47edcf424cd8d727
verhowyna <verhowyna@ukr.net>
2024-04-03 00:03:11 +0300

    first commit

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -7,6 +7,7 @@ import validateBody from "../helpers/validateBody.js";
 import { userSignupSchema, userSigninSchema } from "../schemas/usersSchemas.js";
 
 import authenticate from "../middlewares/authenticate.js";
+import upload from "../middlewares/upload.js";
 
 const authRouter = express.Router();
 
@@ -25,5 +26,12 @@ authRouter.post(
 authRouter.get("/current", authenticate, authController.getCurrent);
 
 authRouter.post("/logout", authenticate, authController.signout);
+
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  authController.updateAvatar
+);
 
 export default authRouter;
